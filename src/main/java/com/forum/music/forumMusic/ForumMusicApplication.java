25d2a2443b3410d2ad0c8ea6ff74c8c796648b31
reinaldo_java <reinaldo.java@gmail.com>
2021-03-13 12:12:05 -0300

    files initial

---
+++ src/main/java/com/forum/music/forumMusic/ForumMusicApplication.java
@@ -0,0 +1,13 @@
+package com.forum.music.forumMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forum/music/forumMusic/ForumMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.music.forumMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
